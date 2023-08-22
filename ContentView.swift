59cc2efc8d5aff84e9b39a9c77ad05ff69f4c07e
Luke Sivulka <lukesiv146@icloud.com>
2023-08-22 10:55:54 -0500

    Initial Commit to git hub

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("jordan")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Jordan Love!")
+        }
+    }
+}
