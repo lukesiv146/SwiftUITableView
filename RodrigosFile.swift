aa26c41000bf2aa5fdd378240a6c68e636a41668
Luke Sivulka <lukesiv146@icloud.com>
2023-08-28 10:48:53 -0500

    create your table view

---
+++ RodrigosFile.swift
@@ -0,0 +1,20 @@
+//
+//  RodrigosFile.swift
+//  SwiftUITableView
+//
+//  Created by Luke J. Sivulka on 8/28/23.
+//
+
+import SwiftUI
+
+struct RodrigosFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RodrigosFile_Previews: PreviewProvider {
+    static var previews: some View {
+        RodrigosFile()
+    }
+}
