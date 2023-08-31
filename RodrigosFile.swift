9a2a8317f9d288bf80b82761da6e1a4c6d3d34c1
Luke Sivulka <lukesiv146@icloud.com>
2023-08-31 13:16:47 -0500

    file is done

--- RodrigosFile.swift
+++ RodrigosFile.swift
@@ -7,55 +7,48 @@
 
 import SwiftUI
 
+
+    struct Schedule: Identifiable {
+    let id = UUID()
+    let name: String
+    let period: String
+    let color: Color
+}
+
 struct RodrigoFile: View {
+    
+    @State private var schedules: [Schedule] = [
+        Schedule(name: "Adv strength & cond", period: "1", color: .red),
+        Schedule(name: "Ap Comp SCI", period: "2", color: .black),
+        Schedule(name: "Algebra 2", period: "3", color: .purple),
+        Schedule(name: "Graphic Arts Design", period: "4", color: .yellow),
+        Schedule(name: "Tutoring Practicum", period: "5", color: .blue),
+        Schedule(name: "Introduction To Rhetoric", period: "6", color: .orange),
+        Schedule(name: "Physics", period: "7", color: .green),
+        Schedule(name: "Independent Learning Time", period: "8", color: .gray)
+    ]
+    
+    @State private var sortOrder = [KeyPathComparator(\Schedule.name)]
+    
     var body: some View {
         
-        
-        struct TableExample: View {
-            struct Schedule: Identifiable {
-                let id = UUID()
-                let name: String
-                let period: String
-                let color: Color
-            }
-            
-            @State private var schedules: [Schedule] = [
-                Schedule(name: "Adv strength & cond", period: "1", color: .red),
-                Schedule(name: "Ap Comp SCI", period: "2", color: .black),
-                Schedule(name: "Algebra 2", period: "3", color: .purple),
-                Schedule(name: "Graphic Arts Design", period: "4", color: .yellow),
-                Schedule(name: "Tutoring Practicum", period: "5", color: .blue,
-                Schedule(name: "Introduction To Rhetoric", period: "6", color: .orange),
-                         Schedule(name: "Physics", period: "7", color: .green),
-                Schedule(name: "Independent Learning Time", period: "8", color: .gray)
-            ]
-            @State private var sortOrder = [KeyPathComparator(\Schedule.name)]
-            
-            var body: some View {
-                NavigationStack {
-                    Table(schedules, sortOrder: $sortOrder) {
-                        TableColumn("", value: \.period)
-                            .width(32)
-                        TableColumn("Name", value: \.name)
-                        TableColumn("Color") { fruit in
-                            Circle()
-                                .frame(width: 16)
-                                .foregroundColor(fruit.color)
-                        }
-                    }
-                    .onChange(of: sortOrder) {
-                        schedules.sort(using: $0)
-                    }
+        NavigationStack {
+            Table(schedules, sortOrder: $sortOrder) {
+                TableColumn("", value: \.period)
+                    .width(32)
+                TableColumn("Name", value: \.name)
+                TableColumn("Color") { fruit in
+                    Circle()
+                        .frame(width: 16)
+                        .foregroundColor(fruit.color)
                 }
             }
-            struct RodrigosFile_Previews: PreviewProvider {
-                static var previews: some View {
-                    
-                    RodrigosFile()
-
+            .onChange(of: sortOrder) {
+                schedules.sort(using: $0)
             }
-            
         }
     }
 }
-}
+
+
+
